445528f7b95ebc075559b493120a352f963b507e
oa <C:\>
2023-03-09 19:00:17 +0600

    home work 3

---
+++ hw3/src/Task1/Items.java
@@ -0,0 +1,14 @@
+package Task1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Items {
+    private String name;
+    private Double price;
+    private Integer sort;
+
+
+}
